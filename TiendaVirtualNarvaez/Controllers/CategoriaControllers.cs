02641a4992c8646449e2791f1d22fc24d2a57ead
Richard N <richardcamilonarvaez@gmail.com>
2026-03-05 15:48:53 -0500

    New Controller

---
+++ TiendaVirtualNarvaez/Controllers/CategoriaControllers.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TiendaVirtualNarvaez.Models;
+
+namespace TiendaVirtualNarvaez.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categoria = new List<Categoria>
+            {
+                new Categoria { Nombre= "Tegnologia", Descripcion="Elementos Tecnologicos", Estado="Activo" },
+                new Categoria { Nombre= "Ropa", Descripcion="Prendas de Ropa", Estado="Activo"},
+                new Categoria { Nombre= "", Descripcion="", Estado="Inactivo"}
+            };
+            return View(categoria);
+        }
+    }
+}
