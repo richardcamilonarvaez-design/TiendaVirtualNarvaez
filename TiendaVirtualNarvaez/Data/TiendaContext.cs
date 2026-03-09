548c1b936c8a7748f2ba60059a56b623fa0220b1
Richard N <richardcamilonarvaez@gmail.com>
2026-03-09 17:49:10 -0500

    New carpeta (Data)

---
+++ TiendaVirtualNarvaez/Data/TiendaContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TiendaVirtualNarvaez.Models;
+
+namespace TiendaVirtualNarvaez.Data
+{
+    public class TiendaContext: DbContext
+    {
+        public TiendaContext(DbContextOptions<TiendaContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Usuario> Clientes { get; set; }
+    }
+}
--- TiendaVirtualNarvaez/Program.cs
+++ TiendaVirtualNarvaez/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using TiendaVirtualNarvaez.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<TiendaContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
