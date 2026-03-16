b670caae36a701fc6b3960e834954cf7dc9fdd75
Richard N <richardcamilonarvaez@gmail.com>
2026-03-16 17:43:04 -0500

    Connection server SQL

--- TiendaVirtualNarvaez/Controllers/ProductoController.cs
+++ TiendaVirtualNarvaez/Controllers/ProductoController.cs
@@ -1,18 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TiendaVirtualNarvaez.Models;
+using TiendaVirtualNarvaez.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace TiendaVirtualNarvaez.Controllers
 {
     public class ProductoController : Controller
     {
+        private readonly TiendaContext _context;
+        public ProductoController(TiendaContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            var productos = new List<Producto>
-            {
-                new Producto { Id = 1,Nombre= "Laptop", Precio=3000, Stock=5 },
-                new Producto { Id = 2,Nombre= "Mouse", Precio=80, Stock=0 }
-            };
-            return View(productos);
+            var productos = _context.Productos
+            .Include(p => p.Categoria) 
+            .ToList();
+            return View(productos); 
         }
     }
 }
